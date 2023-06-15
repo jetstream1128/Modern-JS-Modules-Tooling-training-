640649e071b8ba1dac9d286713eafb086498a687
jetstream1128 <bogdanalexeev2007@live.com>
2023-06-14 18:49:27 -0600

    1

--- starter/script.js
+++ starter/script.js
@@ -1,13 +1,84 @@
+/*
 //Importing module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // console.log(price, tq);
 // addToCart('bread', 5);
 console.log('Importing module');
 
-import * as ShoppingCart from './shoppingCart.js';
-ShoppingCart.addToCart('bread', 5);
-console.log(ShoppingCart.totalPrice);
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
 
 // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
-import add from './shoppingCart.js';
+import add, { cart } from './shoppingCart.js';
 add('sugar', 3);
+add('apple', 5);
+add('juice', 1);
+
+console.log(cart);
+
+// console.log('start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+
+// console.log('something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+//not very clean
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+*/
+/*
+const shoppingCart2 = (function () {
+  const cart = [];
+  const shoppingCart = 10;
+  const totalPrice = 235;
+  const totalQuantity = 33;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} Ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+shoppingCart2.addToCart('apples', 4);
+shoppingCart2.addToCart('pizza', 2);
+
+console.log(shoppingCart2);
+*/
+
+//commonJS
+/*
+//export
+export.addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+//import
+const {addToCart} = require('./shoppingCart.js');
+*/
--- starter/shoppingCart.js
+++ starter/shoppingCart.js
@@ -1,8 +1,13 @@
 //exporting module
 console.log('Exporting module');
 
+//blocking code
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finished fetching');
+
 const shippingCost = 10;
-const cart = [];
+export const cart = [];
 
 export const addToCart = function (product, quantity) {
   cart.push({ product, quantity });
